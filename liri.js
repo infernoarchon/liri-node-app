6008e8d75ca5727a91a2d2d6004542a5c61f1681
Ali C <17682708+infernoarchon@users.noreply.github.com>
2018-12-04 01:16:44 -0500

    chalk tweaks

--- liri.js
+++ liri.js
@@ -1,22 +1,44 @@
+// Requires
 require("dotenv").config();
 const keys = require("./keys.js")
-var axios = require("axios")
-var Spotify = require('node-spotify-api')
+const axios = require("axios")
+const Spotify = require('node-spotify-api')
+const chalk = require('chalk');
 
+// Global Variables
 var spotify = new Spotify(keys.spotify);
 var input = []
+var userquery = []
 
-if(process.argv[2] === "spotify-this-song") {
-    for (var i=3; i < process.argv.length; i++) {
-        input.push(process.argv[i])
-    }
-    spotify.search({ type: 'track', query: input.join(' '), limit: 1 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+// Main App Logic
+var liri = {
+    spotifyquery : function() {
+        spotify.search({ type: 'track', query: userquery, limit: 1 }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
+            }
+        console.log(chalk.underline("Artist") + ": " + data.tracks.items[0].artists[0].name);
+        console.log(chalk.underline("Song") + ": " + data.tracks.items[0].name);
+        console.log(chalk.underline("Preview URL") + ": " + data.tracks.items[0].preview_url);
+        console.log(chalk.underline("Album") + ": " + data.tracks.items[0].album.name);    
+        })
+    },
+    logic : function() {
+        if(process.argv[2] === "spotify-this-song") {
+            if(process.argv[3] == null) {
+                userquery = "The Sign Ace of Base"
+                liri.spotifyquery()
+            }
+            else {
+                for (var i=3; i < process.argv.length; i++) {
+                    input.push(process.argv[i])
+                }
+                userquery = input.join(' ')
+                liri.spotifyquery()   
+            }
         }
-      console.log(data.tracks.items[0].artists[0].name);
-      console.log(data.tracks.items[0].name);
-      console.log(data.tracks.items[0].preview_url);
-      console.log(data.tracks.items[0].album.name);    
-      });
+    }
 }
+
+// Run Logic
+liri.logic()
